63719b53e575ab0454bd37a4fcabc2e628ac9097
feskomaksim1204 <feskomaksim@gmail.com>
2026-02-03 20:49:00 +0300

    Sprint 6

--- src/test/java/com/example/FelineTest.java
+++ src/test/java/com/example/FelineTest.java
@@ -1,37 +1,10 @@
 package com.example;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import java.util.List;
 import static org.junit.Assert.*;
 
-@RunWith(Parameterized.class)
 public class FelineTest {
-
-    private final int expectedKittens;
-    private final int inputCount;
-
-    public FelineTest(int inputCount, int expectedKittens) {
-        this.inputCount = inputCount;
-        this.expectedKittens = expectedKittens;
-    }
-
-    @Parameterized.Parameters
-    public static Object[][] getTestData() {
-        return new Object[][] {
-                {1, 1},
-                {0, 0},
-                {5, 5},
-                {10, 10}
-        };
-    }
-
-    @Test
-    public void testGetKittensWithCount() {
-        Feline feline = new Feline();
-        assertEquals(expectedKittens, feline.getKittens(inputCount));
-    }
 
     @Test
     public void testGetKittensDefault() {
--- src/test/java/com/example/LionParametrizedTest.java
+++ src/test/java/com/example/LionParametrizedTest.java
@@ -1,4 +1,35 @@
 package com.example;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
 public class LionParametrizedTest {
+
+    private final String sex;
+    private final boolean expectedHasMane;
+
+    public LionParametrizedTest(String sex, boolean expectedHasMane) {
+        this.sex = sex;
+        this.expectedHasMane = expectedHasMane;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTestData() {
+        return new Object[][] {
+                {"Самец", true},
+                {"Самка", false}
+        };
+    }
+
+    @Test
+    public void testDoesHaveMane() throws Exception {
+        Predator predatorMock = mock(Predator.class);
+        Lion lion = new Lion(sex, predatorMock);
+        assertEquals(expectedHasMane, lion.doesHaveMane());
+    }
 }
